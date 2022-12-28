558ed53d4898eb8abccb789713372a7314d1c74e
rasbot <nathan.f.rasmussen@gmail.com>
2022-12-28 10:58:02 -0800

    update utils script

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+import os
+from typing import Tuple, List, Callable
 
 
 def get_file_strings(file_path: str, full_path=False) -> Tuple[str, str]:
@@ -22,3 +23,41 @@ def get_file_strings(file_path: str, full_path=False) -> Tuple[str, str]:
         file_name = file_name.rsplit("/", 1)[-1]
     file_ext = file_strings[-1]
     return file_name, file_ext
+
+
+def is_valid_ext(file_path:str, ext_list: List[str]) -> bool:
+    """Check if file has a valid extension.
+
+    Args:
+        file_path (str): File path (ex: mydir/my_file.txt)
+
+    Returns:
+        bool: Bool indicating if the file is a
+            valid file based on the extension.
+    """
+    _, file_ext = get_file_strings(file_path)
+    return file_ext in ext_list
+
+
+def process_all_files(file_dir: str, ext_list: List[str], process_class: Callable, **kwargs) -> None:
+    """Process all files in specified directory. Will process
+    files if they match an extension in the ext_list variable.
+
+    Args:
+        file_dir (str): Directory containing files.
+        ext_list (List[str]): List of valid extensions to check against.
+        process_class (Callable): Specific processing class to use.
+    """
+    file_names = os.listdir(file_dir)
+
+    file_paths = []
+    for file_name in file_names:
+        file_path = os.path.join(file_dir, file_name)
+        if os.path.isfile(file_path):
+            if is_valid_ext(file_path, ext_list):
+                file_paths.append(file_path)
+            else:
+                print(f"{file_path} might not be a video file, not processing it for now.")
+
+    for file_path in file_paths:
+        process_class(file_path, **kwargs).process_file()
