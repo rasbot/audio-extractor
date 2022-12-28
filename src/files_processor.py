623b8d0d85c73e815100ee14d24fd73bed3bcfdf
rasbot <nathan.f.rasmussen@gmail.com>
2022-12-28 10:56:38 -0800

    generalize processor functions and move to new script

---
+++ src/files_processor.py
@@ -0,0 +1,27 @@
+import os
+from typing import List, Callable
+from utils import is_valid_ext
+
+
+def process_all_files(file_dir: str, ext_list: List[str], process_class: Callable, **kwargs) -> None:
+    """Process all files in specified directory. Will process
+    files if they match an extension in the ext_list variable.
+
+    Args:
+        file_dir (str): Directory containing files.
+        ext_list (List[str]): List of valid extensions to check against.
+        process_class (Callable): Specific processing class to use.
+    """
+    file_names = os.listdir(file_dir)
+
+    file_paths = []
+    for file_name in file_names:
+        file_path = os.path.join(file_dir, file_name)
+        if os.path.isfile(file_path):
+            if is_valid_ext(file_path, ext_list):
+                file_paths.append(file_path)
+            else:
+                print(f"{file_path} might not be a video file, not processing it for now.")
+
+    for file_path in file_paths:
+        process_class(file_path, **kwargs).process_file()
