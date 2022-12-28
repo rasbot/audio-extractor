d9a8b43901d19d9ca640647e3ee5c904d2a24940
rasbot <nathan.f.rasmussen@gmail.com>
2022-12-28 13:09:00 -0800

    change kwarg to snake_case

--- src/normalize_audios_from_dir.py
+++ src/normalize_audios_from_dir.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
         "--dBFS", type=int, default=-30, help="Target dBFS. Defaults to -30."
     )
     args = parser.parse_args()
-    process_all_files(args.dir, AUDIO_EXTS, AudioNormalizer, target_dBFS=args.dBFS)
+    process_all_files(args.dir, AUDIO_EXTS, AudioNormalizer, target_dbfs=args.dBFS)
