5bee20f73773e7e30b1e8b80b4a3bc2cdc95bdb9
rasbot <nathan.f.rasmussen@gmail.com>
2022-12-28 13:08:29 -0800

    add all functionality to main script to process a directory of files

--- src/files_processor.py
+++ src/files_processor.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from typing import List, Callable
+from constants import VID_EXTS, AUDIO_EXTS
 from utils import is_valid_ext
+from audio_extractor import AudioExtractor
+from audio_tagger import AudioTagger
+from audio_normalizer import AudioNormalizer
 
 
 def process_all_files(
@@ -29,3 +34,65 @@ def process_all_files(
 
     for file_path in file_paths:
         process_class(file_path, **kwargs).process_file()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dir",
+        type=str,
+        default='./data',
+        help="Directory with files to process."
+    )
+    parser.add_argument(
+        "--extract",
+        action="store_true",
+        help="Extract flag - extracts audio from videos in directory."
+    )
+    parser.add_argument(
+        "--normalize",
+        action="store_true",
+        help="Normalize flag - normalize audio of files in directory."
+    )
+    parser.add_argument(
+        "--tag",
+        action="store_true",
+        help="Tag flag - tag audio from files in directory."
+    )
+    parser.add_argument(
+        "--dBFS",
+        type=int,
+        default=-30,
+        help="Target dBFS. Defaults to -30."
+    )
+    parser.add_argument(
+        "--artist",
+        type=str,
+        default="default artist",
+        help="artist tag for mp3 file - Defaults to 'default artist'.",
+    )
+    parser.add_argument(
+        "--album",
+        type=str,
+        default="default album",
+        help="album tag for mp3 file - Defaults to 'default album'.",
+    )
+    args = parser.parse_args()
+    if args.extract:
+        process_all_files(args.dir, VID_EXTS, AudioExtractor)
+    if args.normalize:
+        if args.extract:
+            audio_dir = './data/extracted_audio'
+        else:
+            audio_dir = args.dir
+        process_all_files(audio_dir, AUDIO_EXTS, AudioNormalizer, target_dbfs=args.dBFS)
+    if args.tag:
+        if args.normalize:
+            audio_dir = './data/normalized_audio'
+        elif args.extract:
+            audio_dir = './data/extracted_audio'
+        else:
+            audio_dir = args.dir
+        process_all_files(
+            audio_dir, AUDIO_EXTS, AudioTagger, artist_tag=args.artist, album_tag=args.album
+        )
