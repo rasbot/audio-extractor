d355b40461be865cef8211256fa96256fb54aa78
Nathan Rasmussen <nathan.f.rasmussen@gmail.com>
2025-08-23 11:09:07 -0700

    add paths

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = REPO_ROOT / "data"
+NORMALIZED_DIR = DATA_DIR / "normalized_audio"
+EXTRACTED_DIR = DATA_DIR / "extracted_audio"
+
 VID_EXTS = [
     "mp4",
     "avi",
